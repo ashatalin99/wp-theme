9416a3f4abd18080f6924bece6448a075d8f0848
Alex Shatalin <ashatalin1981@gmail.com>
2025-03-25 18:08:49 -0400

    feat: add single support and cleanup functions

---
+++ singles/single.php
@@ -0,0 +1,9 @@
+<?php
+the_post();
+/**
+ * To create additional custom post templates
+ * add new file single-#{custom-type}.php
+ * 
+ * To set template name use Template Name: #{name-of-the-template}
+ */
+echo 'This is single post';
